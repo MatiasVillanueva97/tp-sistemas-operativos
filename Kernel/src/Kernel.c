1aaadef1c9a2387f8478aeb031cf1879b27ee4f8
lclauser <lclauser@est.frba.utn.edu.ar>
2017-04-06 23:47:24 -0300

    Configuracion Inicial del Kernel CASI TERMINADA Sacar Hardcodeo

--- Kernel/src/Kernel.c
+++ Kernel/src/Kernel.c
@@ -15,11 +15,63 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include "sockets.h"
+#include "commons/config.h"
 
 
 #define PORT "3490"  // the port users will be connecting to
 
 #define BACKLOG 10	 // how many pending connections queue will hold
+
+
+char* getStringFromConfig(t_config *config, char*valor){
+	char* aux = malloc(sizeof(char*));
+
+	if(config_has_property(config, valor)){
+		strcpy(aux,config_get_string_value(config, valor));
+	}
+
+	else perror("Archivo config mal hecho");
+
+	return aux;
+}
+
+
+typedef struct {
+	char * PUERTO_PROG;
+	char * PUERTO_CPU;
+	char * IP_MEMORIA;
+	char * PUERTO_MEMORIA;
+	char * IP_FS;
+	char * PUERTO_FS;
+	char * QUANTUM;
+	char * QUANTUM_SLEEP;
+	char * ALGORITMO;
+	char * GRADO_MULTIPROG;
+	char ** SEM_IDS;
+	char ** SEM_INIT;
+	char ** SHARED_VARS;
+	char * STACK_SIZE;
+}estructuraDeMierda;
+
+void configuracionInicial(char*PATH, estructuraDeMierda * est){
+	t_config * config;
+	config = config_create(PATH);
+	est->PUERTO_PROG = getStringFromConfig(config,"PUERTO_PROG");
+	est->PUERTO_CPU = getStringFromConfig(config,"PUERTO_CPU");
+	est->IP_MEMORIA = getStringFromConfig(config,"IP_MEMORIA");
+	est->PUERTO_MEMORIA = getStringFromConfig(config,"PUERTO_MEMORIA");
+	est->IP_FS = getStringFromConfig(config,"IP_FS");
+	est->PUERTO_FS = getStringFromConfig(config,"PUERTO_FS");
+	est->QUANTUM = getStringFromConfig(config,"QUANTUM");
+	est->QUANTUM_SLEEP = getStringFromConfig(config,"QUANTUM_SLEEP");
+	est->ALGORITMO = getStringFromConfig(config,"ALGORITMO");
+	est->GRADO_MULTIPROG = getStringFromConfig(config,"GRADO_MULTIPROG");
+	est->SEM_IDS = config_get_array_value(config,"SEM_IDS");
+	est->SEM_INIT = config_get_array_value(config,"SEM_INIT");
+	est->SHARED_VARS = config_get_array_value(config,"SHARED_VARS");
+	est->STACK_SIZE = getStringFromConfig(config,"STACK_SIZE");
+}
+
 
 int main(void)
 {
@@ -33,7 +85,11 @@ int main(void)
 	int rv;
 	char buf[100];
 
-	socket=crearSocketYBindeo(PORT);
+	estructuraDeMierda configuracion;
+
+	configuracionInicial("/home/utnso/workspace/tp-2017-1c-While-1-recursar-grupo-/Kernel/kernel.config",&configuracion);
+
+	socket=crearSocketYBindeo(configuracion.PUERTO_PROG);
 
 	escuchar(socket);
 
