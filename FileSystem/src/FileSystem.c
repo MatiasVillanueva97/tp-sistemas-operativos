c4c9a2572e7cad04c2efd46a79f7db6c34661898
lclauser <lclauser@est.frba.utn.edu.ar>
2017-04-06 21:34:28 -0300

    Ahora el File System lee su archivo config Como siempre ver errores de memory leaks

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -10,8 +10,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "commons/config.h"
 
-int main(void) {
-	puts("puto el que lee"); /* prints !!!Hello World!!! */
+#include <arpa/inet.h>
+
+char* getStringFromConfig(t_config *config, char*valor){
+	char* aux = malloc(sizeof(char*));
+
+	if(config_has_property(config, valor)){
+		strcpy(aux,config_get_string_value(config, valor));
+	}
+
+	else perror("Archivo config mal hecho");
+
+	return aux;
+}
+
+void configuracionInicial(char*PATH,char**PORT,char**PUNTO_MONTAJE){
+	t_config *config;
+	config = config_create(PATH);
+	*PORT = getStringFromConfig(config,"PUERTO");
+	*PUNTO_MONTAJE = getStringFromConfig(config,"PUNTO_MONTAJE");
+	config_destroy(config);
+}
+
+
+int main(int argc, char* argv[]) {
+
+	char *PORT,*PUNTO_MONTAJE;
+
+	if (argc != 2) {
+		    fprintf(stderr,"usage: client hostname\n");
+		    exit(1);
+		}
+
+	configuracionInicial(argv[1],&PORT,&PUNTO_MONTAJE);
+
+
 	return EXIT_SUCCESS;
 }
