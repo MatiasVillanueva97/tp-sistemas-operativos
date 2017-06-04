251f04cb7097e4cabfe784e064b875af3a0d7b7a
SantiagoParedes97 <sparedes@est.frba.utn.edu.ar>
2017-06-04 00:19:47 -0300

    Pusheo la funcion nueva del dividir el script en paginas, deje la otra, la nueva se llama igual solo que con un 2 al final. Lean el comentario que tiene. Por si les da paja, necesita un puntero a int y el script. Y te devuelve en el puntero la cantidad de paginas, y ademas, el script dividido. Pruebenlo si quieren, por mi probe los casos mas basicos y anda.

--- Kernel/src/funcionesMemoria.c
+++ Kernel/src/funcionesMemoria.c
@@ -26,6 +26,45 @@ char** memoria_dividirScriptEnPaginas(int cant_paginas, char *copiaScriptAnsisop
 	}
 	return scriptDivididoEnPaginas;
 }
+char** memoria_dividirScriptEnPaginas2(int *cant_paginas, char *copiaScriptAnsisop) //Devuelve el script dividio y en el int que pasas por parametro la cantidadDePaginas
+{
+	char ** scriptDivididoEnPaginas = malloc(sizeof(char*));
+	char* pagina = string_new();
+	char** instrucciones = string_split(copiaScriptAnsisop,"\n");
+	int numeroPaginaActual = 0;
+	int tamano= 0;
+	int i;
+	for(i=0;*(instrucciones+i);i++){
+		if(strlen(*(instrucciones+i)) > size_pagina){ // Por si en algun momento te tiran una instruccion de mas del size de pagina
+			free(instrucciones);
+			free(pagina);
+			free(scriptDivididoEnPaginas);
+			return NULL;
+		}
+		if(tamano+strlen(*(instrucciones+i)) >size_pagina){
+			*(scriptDivididoEnPaginas+numeroPaginaActual) = malloc(size_pagina);
+			strcpy(*(scriptDivididoEnPaginas+numeroPaginaActual),pagina);
+			numeroPaginaActual++;
+			tamano = 0;
+			pagina = *(instrucciones+i);
+			string_append(&pagina,"\n");
+		}
+		else{
+			string_append(&pagina,*(instrucciones+i));
+			//Si al negro le molesta, se puede sacar el ultimo \n y dejarlo vacio
+			string_append(&pagina,"\n");
+			tamano +=strlen(*(instrucciones+i));
+		}
+	}
+	if (tamano!= 0){
+		*(scriptDivididoEnPaginas+numeroPaginaActual) = malloc(size_pagina);
+		strcpy(*(scriptDivididoEnPaginas+numeroPaginaActual),pagina);
+	}
+	*cant_paginas = numeroPaginaActual+1; // Empieza en 0 la variable, por eso le sumo 1
+	free(pagina);
+	free(instrucciones);
+	return scriptDivididoEnPaginas;
+}
 
 /// *** Esta función esta probada y anda
 int memoria_CalcularCantidadPaginas(char * scriptAnsisop)
