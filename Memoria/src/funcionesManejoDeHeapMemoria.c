f26736bcb38073b495718c4f6deb272aecabc36f
gabrielspisso <ghs_9507@hotmail.com>
2017-06-14 00:56:02 -0300

    Revert "Esto es una verga"
    This reverts commit a0e4e218b15beec0aa6073cea226f81fed21a409.

--- Memoria/src/funcionesManejoDeHeapMemoria.c
+++ Memoria/src/funcionesManejoDeHeapMemoria.c
@@ -39,10 +39,9 @@ void* leerMemoria(int posicion_dentro_de_la_pagina, int*tamanioStreamLeido){
 	}
 }
 //Esto es para heap y sufrira modificaciones
-void* escribirMemoria(void* contenido,int tamano,void* memoria){
+int escribirMemoria(void* contenido,int tamano,void* memoria){
 
-	HeapMetadata headerAnterior = *((HeapMetadata*) memoria);
-	void* buffer = malloc(tamano+sizeof(HeapMetadata)*2);
+	HeapMetadata headerAnterior = *((HeapMetadata*) memoriaTotal);
 	int recorrido=0;
 		recorrido = recorrido +tamanoHeader;//El recorrido se posiciona en donde termina el header.
 	while(sizeOfPaginas > recorrido){
@@ -51,69 +50,42 @@ void* escribirMemoria(void* contenido,int tamano,void* memoria){
 				header.isFree= false;
 				header.size= tamano;
 				int y = recorrido-tamanoHeader;
-				int offset = recorrido;
-				memcpy(memoria+y,&header,tamanoHeader);
-				memcpy(memoria+recorrido,contenido,tamano);
+				memcpy(memoriaTotal+y,&header,tamanoHeader);
+				memcpy(memoriaTotal+recorrido,contenido,tamano);
 				recorrido += tamano; //El recorrido se posiciona donde va el siguiente header
 				header.isFree= true;
 				header.size= headerAnterior.size-tamano-tamanoHeader; //Calculo el puntero donde esta, es decir, el tamaño ocupado, y se lo resto a lo que queda.
-				memcpy(memoria+recorrido,&header,tamanoHeader);
-				memcpy(buffer,memoria+offset-sizeof(HeapMetadata),tamano+sizeof(HeapMetadata)*2);
-				if(offset == 5){
-					offset = 0;
-				}
-				printf("El offset es: %d", offset);
+				memcpy(memoriaTotal+recorrido,&header,tamanoHeader);
 				//escribir en la memoria
-				return buffer;
+				return 1;
 		}
 		else{
 			recorrido+=headerAnterior.size;//el header se posiciona para leer el siguiente header.
 		}
 		headerAnterior = *((HeapMetadata*) (memoria+recorrido));
-		recorrido+=tamanoHeader;
 	}
 	return 0;//no hay espacio suficiente
 }
 //Esto es para heap y sufrira modificaciones
-void liberarMemoriaHeap(int offset,void* pagina){
-	if (offset<0){
+void liberarMemoria(int posicion_dentro_de_la_pagina,void* pagina){
+	if (posicion_dentro_de_la_pagina<0){
 			perror("ingreso una posicion de la pagina negativa.");
 	}
 	int i;
 	int recorrido = 0;
-	HeapMetadata *headerActual = ((HeapMetadata*) (pagina+recorrido));
-	HeapMetadata *headerAnterior = malloc(tamanoHeader);
+	HeapMetadata x = *((HeapMetadata*) (pagina+recorrido));
+	recorrido+= sizeof(x);
+	for (i=0;i<posicion_dentro_de_la_pagina&&recorrido<sizeOfPaginas;i++){
+			recorrido+=x.size;
+			x = *((HeapMetadata*) (pagina+recorrido));
+			recorrido+= sizeof(x);
 
-	int offsetQueTengoQueDevolver = 0;
-
-	headerAnterior->isFree = false;
-	int recorridoDesdeDondeTengoQueCopiar=recorrido;
-	recorrido+= tamanoHeader;
-	while(recorrido<sizeOfPaginas&&recorrido+sizeof(HeapMetadata)<offset){
-		recorridoDesdeDondeTengoQueCopiar = recorrido-5;
-		headerAnterior = headerActual;
-		recorrido+=headerActual->size;
-		headerActual = ((HeapMetadata*) (pagina+recorrido));
-		recorrido+= tamanoHeader;
 	}
-		if(recorrido>=sizeOfPaginas){
+	if(recorrido>=sizeOfPaginas){
 			perror("pidio una posicion invalida, es decir, que es mayor al numero de posiciones dentro de la pagina"); // esto significa posicion invalida
 	}
 	else{
-			headerActual->isFree= true;
-			HeapMetadata* headerSiguiente;
-			HeapMetadata  headerAEscribir = *headerActual;
-			headerSiguiente = ((HeapMetadata*) (pagina+recorrido+headerActual->size));
-			if(headerSiguiente->isFree){
-				headerActual->size += headerSiguiente->size + tamanoHeader;
-				offsetQueTengoQueDevolver = recorrido;
-			}
-			if(headerAnterior->isFree){
-				headerAnterior->size += headerActual->size + tamanoHeader;
-				offsetQueTengoQueDevolver = recorridoDesdeDondeTengoQueCopiar;
-				headerAEscribir = *headerAnterior;
-			}
-
-			//tengo que devolver offsetQueTengoQueDevolver y el headerQueTengaQueEscribir
+			x.isFree= true;
+			memcpy(pagina+recorrido-sizeof(x),&x,sizeof(x));
 	}
 }
